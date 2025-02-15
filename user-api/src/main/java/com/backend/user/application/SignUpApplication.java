1c6b91865788f8c2e883b58c78470806f6b2ef21
Jinseonghyun <jinsunghyun9@gmail.com>
2025-02-15 14:41:18 +0900

    feat : [003-EMAIL-VERIFICATION] 회원가입 후 이메일 전송에 대한 실제 연동

--- user-api/src/main/java/com/backend/user/application/SignUpApplication.java
+++ user-api/src/main/java/com/backend/user/application/SignUpApplication.java
@@ -1,27 +1,58 @@
 package com.backend.user.application;
 
 import com.backend.user.client.MailgunClient;
+import com.backend.user.client.mailgun.SendMailForm;
 import com.backend.user.domain.SignUpForm;
 import com.backend.user.domain.model.Customer;
 import com.backend.user.exception.CustomException;
 import com.backend.user.exception.ErrorCode;
 import com.backend.user.service.SignUpCustomerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class SignUpApplication {
 
     private final MailgunClient mailgunClient;
     private final SignUpCustomerService signUpCustomerService;
 
-    public void customerSignUp(SignUpForm form) {
+    public String customerSignUp(SignUpForm form) {
         if (signUpCustomerService.isEmailExist(form.getEmail())) {
             throw new CustomException(ErrorCode.ALREADY_REGISTER_USER);
         } else {
             Customer customer = signUpCustomerService.signUp(form);
+            LocalDateTime now = LocalDateTime.now();
+
+            String code = getRandomCode();
+            SendMailForm sendMailForm = SendMailForm.builder()
+                    .from("USER@sandboxf52b5a538f344185a7a8fbdc20b37fd9.mailgun.org")
+                    .to(form.getEmail())
+                    .subject("Verification Email!")
+                    .text(getVerificationEmailBody(customer.getEmail(), customer.getName(), code))
+                    .build();
+            log.info("Send mail result: " + mailgunClient.sendEmail(sendMailForm));
+
+            signUpCustomerService.changeCustomerValidateEmail(customer.getId(), code);
+            return "회원 가입에 성공하였습니다.";
         }
     }
 
+    private String getRandomCode() {
+        return RandomStringUtils.random(10, true, true);
+    }
+
+    private String getVerificationEmailBody(String email, String name, String code) {
+        StringBuilder builder = new StringBuilder();
+        return builder.append("Hello").append(name).append("! Please Click Link for verification.\n\n")
+                .append("http://localhost:8081/customer/signup/verify?email=")
+                .append(email)
+                .append("&code=")
+                .append(code).toString();
+    }
 }
--- user-api/src/main/java/com/backend/user/service/SignUpCustomerService.java
+++ user-api/src/main/java/com/backend/user/service/SignUpCustomerService.java
@@ -3,10 +3,15 @@ package com.backend.user.service;
 import com.backend.user.domain.SignUpForm;
 import com.backend.user.domain.model.Customer;
 import com.backend.user.domain.repository.CustomerRepository;
+import com.backend.user.exception.CustomException;
+import com.backend.user.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Locale;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +26,19 @@ public class SignUpCustomerService {
     public boolean isEmailExist(String email) {
         return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT))
                 .isPresent();
+    }
+
+    @Transactional
+    public LocalDateTime changeCustomerValidateEmail(Long customerId, String verificationCode) {
+        Optional<Customer> customerOptional = customerRepository.findById(customerId);
+
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+            customer.setVerificationCode(verificationCode);
+            customer.setVerifyExpiredAt(LocalDateTime.now().plusDays(1));
+            return customer.getVerifyExpiredAt();
+        }
+        throw new CustomException(ErrorCode.NOT_FOUND_USER);
+
     }
 }
