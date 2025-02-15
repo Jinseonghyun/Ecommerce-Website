06824c778d370083cca68ca6b7e7c059c2c3e940
Jinseonghyun <jinsunghyun9@gmail.com>
2025-02-15 15:08:51 +0900

    feat : [003-EMAIL-VERIFICATION] 이메일을 통한 회원가입 이후 인증 절차 구현

--- user-api/src/main/java/com/backend/user/application/SignUpApplication.java
+++ user-api/src/main/java/com/backend/user/application/SignUpApplication.java
@@ -22,6 +22,10 @@ public class SignUpApplication {
     private final MailgunClient mailgunClient;
     private final SignUpCustomerService signUpCustomerService;
 
+    public void customerVerify(String email, String code) {
+        signUpCustomerService.verifyEmail(email, code);
+    }
+
     public String customerSignUp(SignUpForm form) {
         if (signUpCustomerService.isEmailExist(form.getEmail())) {
             throw new CustomException(ErrorCode.ALREADY_REGISTER_USER);
@@ -50,7 +54,7 @@ public class SignUpApplication {
     private String getVerificationEmailBody(String email, String name, String code) {
         StringBuilder builder = new StringBuilder();
         return builder.append("Hello").append(name).append("! Please Click Link for verification.\n\n")
-                .append("http://localhost:8081/customer/signup/verify?email=")
+                .append("http://localhost:8081/signup/verify/customer/?email=")
                 .append(email)
                 .append("&code=")
                 .append(code).toString();
--- user-api/src/main/java/com/backend/user/service/SignUpCustomerService.java
+++ user-api/src/main/java/com/backend/user/service/SignUpCustomerService.java
@@ -29,6 +29,21 @@ public class SignUpCustomerService {
     }
 
     @Transactional
+    public void verifyEmail(String email, String code) {
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+
+        if (customer.isVerify()) {
+            throw new CustomException(ErrorCode.ALREADY_VERIFY);
+        } else if (!customer.getVerificationCode().equals(code)) {
+            throw new CustomException(ErrorCode.WRONG_VERIFICATION);
+        } else if (customer.getVerifyExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new CustomException(ErrorCode.EXPIRE_CODE);
+        }
+        customer.setVerify(true);
+    }
+
+    @Transactional
     public LocalDateTime changeCustomerValidateEmail(Long customerId, String verificationCode) {
         Optional<Customer> customerOptional = customerRepository.findById(customerId);
 
