579a5f001a1083ea9f4a5c1956264941587eec26
Jinseonghyun <jinsunghyun9@gmail.com>
2025-02-16 20:00:41 +0900

    feat : [005-SELLER_SIGN_UP_IN] customer 와 다르게 SignUpSellerService 를 따로 구현 하지 않고 SellerService 하나에 로직 모두 구현

--- user-api/src/main/java/com/backend/user/domain/repository/SellerRepository.java
+++ user-api/src/main/java/com/backend/user/domain/repository/SellerRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface SellerRepository extends JpaRepository<Seller, Long> {
     Optional<Seller> findByIdAndEmail(Long id, String email);
     Optional<Seller> findByEmailAndPasswordAndVerifyIsTrue(String email, String password);
+    Optional<Seller> findByEmail(String email);
 }
