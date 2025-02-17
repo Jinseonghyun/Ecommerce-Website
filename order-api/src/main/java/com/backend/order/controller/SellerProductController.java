bdb98afd7e54797077624a3bdf4c406e3a16b7f8
Jinseonghyun <jinsunghyun9@gmail.com>
2025-02-17 21:35:25 +0900

    feat : [009-Product_ADD] ProductItem 만 추가 되는 서비스 API 구현

--- order-api/src/main/java/com/backend/order/controller/SellerProductController.java
+++ order-api/src/main/java/com/backend/order/controller/SellerProductController.java
@@ -2,7 +2,9 @@ package com.backend.order.controller;
 
 import com.backend.domain.config.JwtAuthenticationProvider;
 import com.backend.order.domain.product.AddProductForm;
+import com.backend.order.domain.product.AddProductItemForm;
 import com.backend.order.domain.product.ProductDto;
+import com.backend.order.service.ProductItemService;
 import com.backend.order.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class SellerProductController {
 
     private final ProductService productService;
+    private final ProductItemService productItemService;
     private final JwtAuthenticationProvider jwtAuthenticationProvider;
 
     @PostMapping
@@ -21,5 +24,12 @@ public class SellerProductController {
                                            @RequestBody AddProductForm form) {
 
         return ResponseEntity.ok(ProductDto.from(productService.addProduct(jwtAuthenticationProvider.getUserVo(token).getId(), form)));
+    }
+
+    @PostMapping("/item")
+    public ResponseEntity<ProductDto> addProductItem(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                                     @RequestBody AddProductItemForm form) {
+
+        return ResponseEntity.ok(ProductDto.from(productItemService.addProductItem(jwtAuthenticationProvider.getUserVo(token).getId(), form)));
     }
 }
